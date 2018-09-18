cfa7117fd7fb47a87fdf5fb1c34cd24691c113f0
github username <github email address>
2018-09-18 12:11:01 -0500

    Done.

--- errors.js
+++ errors.js
@@ -2,19 +2,23 @@
 
 const snackSelection = 'Pretzels';
 
-if (snackSelection = 'Pretzels') {
+if (snackSelection === 'Pretzels') {
   console.log("That'll be $1, please!");
 }
 
 
 // Fix the code above this line to see the error from this first() function in your browser's console
 
-function first () {
+function first() {
   second();
 }
 
-function second () {
+function second() {
   third();
+}
+
+function third() {
+ console.log("Now I'm a function!");
 }
 
 first();
